fbaeb8a17e7c49ee066eda76045380f6f2b43d00
Samson <et980.0260@yahoo.com>
2022-06-04 21:17:31 -0700

    update

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+import calculator_1
+
+args = len(argv)
+ops = {
+        '+': calculator_1.add,
+        '-': calculator_1.sub,
+        '*': calculator_1.mul,
+        '/': calculator_1.div
+        }
+if args != 4:
+    print("Usage: {} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+a = int(argv[1])
+b = int(argv[3])
+op = argv[2]
+
+if op not in '+-*+/':
+    print("Unknown operator, Available operators: +, -, *, and /")
+    exit(1)
+res = ops[op](a,b)
+print("{:d} {:s} {:d} = {:d}".format(a, op, b, res))
