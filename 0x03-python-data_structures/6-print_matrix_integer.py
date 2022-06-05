f6958ba3fb8014334939d9d21b02b0942b20d001
Samson <et980.0260@yahoo.com>
2022-06-05 10:26:47 -0700

    update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for ls in matrix:
+        i = 0
+        for elem in ls:
+            if i != 0:
+                print(" ", end='')
+            print("{:d}".format(elem),end="")
+            i += 1
+        print()
