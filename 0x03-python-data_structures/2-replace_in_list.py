860a5759596a792e7d10d822c789f032f99970c4
Samson <et980.0260@yahoo.com>
2022-06-05 03:05:54 -0700

    update

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    list_elem = len(my_list)
+
+    if not idx < 0 or idx > (list_elem - 1):
+            my_list[idx] = element
+            return(my_list)
