3c7429b79b41c2a4edb57b0149332eb1e7ab8c34
Samson <et980.0260@yahoo.com>
2022-06-05 09:20:09 -0700

    updat

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    
+    leng = len(my_string)
+
+    new_string = ""
+    for i in range(leng):
+        if not(my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string += my_string[i]
+    return(new_string)
+            
