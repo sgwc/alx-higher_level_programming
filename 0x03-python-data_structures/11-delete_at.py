46202a206c95fd5f6eab38e23c07cce3a9e6a5f5
sgwc <45393676+sgwc@users.noreply.github.com>
2022-06-07 12:43:54 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     leng = len(my_list)
 
-    if idx > 0 and idx <= leng:
+    if idx >= 0 and idx < leng:
         del my_list[idx]
         return(my_list)
     else:
