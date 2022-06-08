a3aea768977339f54e7650fe74c0596fc00463eb
sgwc <45393676+sgwc@users.noreply.github.com>
2022-06-08 22:54:26 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqared_matrix = [[row[i]**2 for i in range(3)] for row in matrix]
+    sqared_matrix = [[row[i]**2 for i in range(len(row))] for row in matrix]
 
     return sqared_matrix
