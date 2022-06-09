172c3d44f837044fcc695b290b3049dc492b0f7c
sgwc <45393676+sgwc@users.noreply.github.com>
2022-06-09 16:44:30 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_el = {key: value}
-    a_dictionary.update(new_el)
-    
+    a_dictionary[key] = value
+    return a_dictionary
