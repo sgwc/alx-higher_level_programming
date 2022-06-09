e7c276a6d49ef7681ed19e95f1d9fe4fa3ebde8c
sgwc <45393676+sgwc@users.noreply.github.com>
2022-06-09 08:38:25 +0300

    Create 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = my_list[0]
+    my_list.sort()
+    prev = my_list[0]
+    for i in range(1,len(my_list)):
+        if my_list[i] == prev:
+            pass
+        else:
+            sum += my_list[i]
+        prev = my_list[i]
+    return sum
