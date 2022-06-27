53c9d19a98a0ae069796d4d08ffff236a79623a8
Samson <et980.0260@yahoo.com>
2022-06-27 13:52:12 -0700

    updat

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Write Rectangle that defines
+    a rectangle by (based on 0-rectangle)"""
+
+
+class Rectangle:
+    """Class Rectangle 0-rectangle"""
+
+    number_of_instances = 0
+    """Public class attribute number_of_instances"""
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        '''Initializes the method'''
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """prints in stdout the rectangle with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height - 1):
+            print(str(self.print_symbol) * self.__width)
+        return (str(self.print_symbol) * self.__width)
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
