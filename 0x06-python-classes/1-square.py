5d58673439fadd29db8b0ced1908f1c6784eeabd
Samson <et980.0260@yahoo.com>
2022-06-29 04:50:23 -0700

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class based 0-square"""
+
+
+class Square:
+    """define Square class"""
+    def __init__(self, size):
+        """initialize Square size"""
+        self.size = size
