5a559b1b410e5cccb67f7258b0eb0071c2aa02d6
sgwc <45393676+sgwc@users.noreply.github.com>
2022-07-05 05:24:57 +0300

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write an class
+"""
+
+
+class BaseGeometry:
+    """Class that defines a shape
+    """
+
+    pass
+
+    def area(self):
+        """Calculates Area
+        """
+
+        raise Exception("area() is not implemented")
