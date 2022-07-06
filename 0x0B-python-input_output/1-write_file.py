beb865f45bb06f2010f2c9f3bb7fa6f97dd73b0d
Samson <et980.0260@yahoo.com>
2022-07-06 16:45:10 -0700

    create s

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module write a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a text file (UTF-8)
+    """
+    with open(filename, 'w',encoding="utf-8") as f:
+       characters = f.write(text)
+       return(characters)
