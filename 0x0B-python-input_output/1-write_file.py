045a65f0c443156d7faff34731847ee8d30a1180
Samson <et980.0260@yahoo.com>
2022-07-06 16:47:06 -0700

    create s

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,6 @@
 def write_file(filename="", text=""):
     """function that writes a text file (UTF-8)
     """
-    with open(filename, 'w',encoding="utf-8") as f:
-       characters = f.write(text)
-       return(characters)
+    with open(filename, 'w', encoding="utf-8") as f:
+        characters = f.write(text)
+        return(characters)
