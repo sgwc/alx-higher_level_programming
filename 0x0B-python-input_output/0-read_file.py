7038eb338b13d58fa8fd598fb4d2a56264dc5d82
Samson <et980.0260@yahoo.com>
2022-07-06 16:20:34 -0700

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Module read a file
+"""
+
+
 def read_file(filename=""):
+    """function that reads a text file (UTF-8)
+    and prints it to the stdout
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read())
