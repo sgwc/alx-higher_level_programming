498b98aa2d7a8640167f98627cd3e8e05752cd4f
Samson <et980.0260@yahoo.com>
2022-07-06 18:36:55 -0700

    create 4-main.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module string representation from json
+"""
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object
+    represented by a JSON string
+    """
+    return (json.loads(my_str))
