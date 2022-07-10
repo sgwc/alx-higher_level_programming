07cab53037e1d2479a68abe8ef5849db4958650d
Samson <et980.0260@yahoo.com>
2022-07-10 01:25:57 -0700

    create base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+           id = __nb_objects++
