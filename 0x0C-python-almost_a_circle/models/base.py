6ed8b8da68f8690a12a6de5eaf186de4d165eceb
Samson <et980.0260@yahoo.com>
2022-07-11 08:48:44 -0700

    format models/base.py as pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,15 +8,14 @@ class Base:
     """ base Class """
     __nb_objects = 0
 
-
     def __init__(self, id=None):
         """initiation method"""
 
         if id is not None:
             self.id = id
         else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     def integer_validator(self, name, value):
         """ check if value is an integer """
@@ -24,12 +23,10 @@ class Base:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be > 0'.format(name))
-    
+
     def integer_validator2(self, name, value):
         """ check if value is an integer """
         if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value < 0:
             raise ValueError('{} must be >= 0'.format(name))
-
-
