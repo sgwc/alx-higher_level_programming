21b7e0d75fa99956d6e9e95ae74f0efa6b186141
Samson <et980.0260@yahoo.com>
2022-07-13 12:09:30 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,6 +38,11 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """return dict representation of Rectangle"""
+        return {'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width}
+
     def area(self):
         """ computes area of the rectangle """
         return self.width * self.height
