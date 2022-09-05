2745c6b154e5b2c51941f052e919a1680543454c
Samson <et980.0260@yahoo.com>
2022-09-05 09:07:45 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const allArgs = process.argv;
-if (allArgs.length < 3)
-{
-	console.log('No arguments');
-}
-else
-{
-	console.log('Arguments found');
+if (allArgs.length < 3) {
+  console.log('No arguments');
+} else {
+  console.log('Arguments found');
 }
