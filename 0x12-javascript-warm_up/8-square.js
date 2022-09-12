5d5c1b1ec1e2551776a29644b0061f78e604e945
Samson <et980.0260@yahoo.com>
2022-09-12 08:07:29 -0700

    update 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const loopArgs = process.argv[2];
+if (!parseInt(loopArgs)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < loopArgs; x++) {
+	  console.log('X'.repeat(loopArgs));
+  }
+}
