e726dfb348dcd98bf9eab80e920d76db78fb4809
Samson <et980.0260@yahoo.com>
2022-09-12 07:43:43 -0700

    create 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const loopArgs = process.argv[2];
+if (!loopArgs) {
+	console.log('Missing number of occurrences');
+} else {
+	for ( let x = 0; x < loopArgs; x++) {
+		console.log('C is fun');
+	}
+}
