6015cd291637e3925565aa8f36fcd9510ba74a27
Samson <et980.0260@yahoo.com>
2022-09-12 07:47:54 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const loopArgs = process.argv[2];
 if (!loopArgs) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for ( let x = 0; x < loopArgs; x++) {
-		console.log('C is fun');
-	}
+  for (let x = 0; x < loopArgs; x++) {
+    console.log('C is fun');
+  }
 }
