f209a6533e49bf0045e340073fe82ad1882b7b0c
Samson <et980.0260@yahoo.com>
2022-09-12 07:50:50 -0700

    update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const { argv } = require('process');
 const argc = argv.length;
 
 if (argc === 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (argc === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('Argument found');
 }
